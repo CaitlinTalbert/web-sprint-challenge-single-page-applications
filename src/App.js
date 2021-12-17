d89a051193be51c9d4957d767aa80d6b13f08375
CaitlinTalbert <92820628+CaitlinTalbert@users.noreply.github.com>
2021-12-17 10:23:15 -0600

    test 1 pass, homepage has an order pizza button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from "react";
+import {Switch, Route, Link} from 'react-router-dom'; 
 
 const App = () => {
+
+
+
+
+
+
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <div className="App">
+      <Switch>
+        <Route exact path="/">
+          <form>
+            <label>
+              <button id="order-pizza"type="submit">Order Pizza!</button>
+            </label>
+          </form>
+        </Route>
+      </Switch>
+    </div>
   );
+
+
+
+
+
 };
 export default App;
