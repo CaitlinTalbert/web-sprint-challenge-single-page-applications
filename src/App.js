00e0994be34b445bf2c014113c4048f8599be46c
CaitlinTalbert <92820628+CaitlinTalbert@users.noreply.github.com>
2021-12-17 10:42:25 -0600

    added header, home page link, form link with a /pizza form and route and form id

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {Switch, Route, Link} from 'react-router-dom'; 
+import './App.css';
 
 const App = () => {
 
@@ -10,12 +11,24 @@ const App = () => {
 
   return (
     <div className="App">
+      <header>
+        <h1>Lambda Eats</h1>
+        <nav>
+          <Link to="/">Home Page</Link>
+          <Link to="/pizza">Form</Link>
+        </nav>
+      </header>
       <Switch>
         <Route exact path="/">
           <form>
             <label>
               <button id="order-pizza"type="submit">Order Pizza!</button>
             </label>
+          </form>
+        </Route>
+        <Route exact path="/pizza">
+          <form id="pizza-form">
+            this is the pizza form
           </form>
         </Route>
       </Switch>
