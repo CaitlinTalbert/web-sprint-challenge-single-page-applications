f382b124f441577f16eddf2230b387198350712d
CaitlinTalbert <92820628+CaitlinTalbert@users.noreply.github.com>
2021-12-17 11:45:07 -0600

    added state to app, two tests were passing, now its saying all are failing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import React from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 import {Switch, Route, Link} from 'react-router-dom'; 
 import './App.css';
 
+const initialFormState = {
+  name: '', 
+  size: '', 
+  topping1: false, 
+  topping2: false, 
+  special: ''
+}
+
+
+
+
+
+
 const App = () => {
 
+  const [pizzaForm, setPizzaForm] = useState([])
+  const [form, setForm] = useState(initialFormState)
 
+  useEffect(() => {
+    axios.get('https://reqres.in/api/orders')
+      .then((res) => {
+        console.log(res.data.data)
+        setPizzaForm(res.data.data)
+      })
+  }, [])
 
-
+  const changeHandler = (event) => {
+    console.log(event.target.name, event.target.value)
+  }
 
 
   return (
@@ -22,14 +47,27 @@ const App = () => {
         <Route exact path="/">
           <form>
             <label>
-              <button id="order-pizza"type="submit">Order Pizza!</button>
+              <input 
+              onChange={changeHandler}
+              name="name" 
+              value={form.name} 
+              id="name-input" 
+              type="text">
+              </input>
             </label>
+            <button id="order-pizza"type="submit">Order Pizza!</button>
           </form>
         </Route>
         <Route exact path="/pizza">
           <form id="pizza-form">
-            this is the pizza form
-          </form>
+            {
+              pizzaForm.map((pizzaForm, index) => {
+                return (
+                  <p key={`formId${pizzaForm.id}`}>{pizzaForm.name}</p>
+                )
+              })
+            }
+            </form>
         </Route>
       </Switch>
     </div>
